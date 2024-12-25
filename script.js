06865871e4bd6dc74ccd7d585003bed062a94600
madam2606 <immfroduction@gmail.com>
2024-12-25 16:05:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Here you can add interactivity if needed
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Pesan berhasil dikirim!');
+});
